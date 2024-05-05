f04e83f3fa08ec4c74f899ea2adffe4129036eaa
Balaji-7 <83109611+Balaji-7@users.noreply.github.com>
2024-05-05 23:46:27 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatalistComponent } from './datalist/datalist.component';
+import { DetailviewComponent } from './detailview/detailview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DatalistComponent},
+  {path:'detailview',component:DetailviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
